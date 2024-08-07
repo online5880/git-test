8d1d882f4eabd8ad163b4260d33bef709385970c
mane <58097724+online5880@users.noreply.github.com>
2024-08-07 21:17:08 +0900

    Update index.js

--- WeatherApp/index.js
+++ WeatherApp/index.js
@@ -6,7 +6,7 @@ const error404 = document.querySelector('.not-found');
 
 search.addEventListener('click', () => {
 
-    const APIKey = '87b173240bc0ad3142684a58b4b02d01';
+    const APIKey = '';
     const city = document.querySelector('.search-box input').value;
 
     if (city === '')
